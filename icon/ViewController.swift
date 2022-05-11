836368a25150475352ae9f4ef4431b749e215e3b
Atsuhiro Muroyama <atuhiro.duda@gmail.com>
2022-05-11 23:17:15 +0900

    Initial Commit

---
+++ icon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  icon
+//
+//  Created by Atsuhiro Muroyama on 2022/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
